6fbe67b5b2dc318ebb5f1d9bc23b38e951b633f2
lauLanez <laulanyumbayire@gmail.com>
2024-06-01 20:25:47 -0500

    first commit

---
+++ Semi_/src/Components/Navbar.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+import logo from '../assets/logo.png'
+
+
+
+import { FaXmark } from "react-icons/fa6";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
+import { Link } from 'react-scroll'
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    const navItems = [
+        { link: "Home", path: "home" },
+        { link: "About Us", path: "feature" },
+        { link: "News", path: "about" },
+        { link: "How to join", path: "pricing" },
+    ];
+
+    return (
+        <nav className='bg-white md:px-14 p-4 max-w-screen-2xl border-b mx-auto text-primary fixed top-0 right-0 left-0 z-50 cursor-pointer'>
+            <div className='text-lg container mx-auto flex justify-between items-center font-medium'>
+                <div className='flex space-x-14 items-center'>
+                    <a href="/" className='text-3xl font-semibold flex items-center space-x-3 text-primary'>
+                        <img src={logo} alt="" className='w-12 inline-block items-center' />
+                       
+                    </a>
+                    <ul className='md:flex space-x-12 hidden'>
+                        {navItems.map(({ link, path }) => (
+                            <li key={link}>
+                                <Link activeClass='active' spy={true} smooth={true} offset={-100} className='block hover:text-gray-300' to={path}>{link}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className='space-x-12 hidden md:flex items-center'>
+                    <a href="/" className='hidden lg:flex items-center hover:text-secondary'>
+                        <FaPhoneAlt  className='mr-2' />Contact us
+                    </a>
+                    {/* <button className='bg-secondary py-2 px-4 transition-all duration-300 rounded hover:text-white hover:bg-indigo-600'>Sign Up</button> */}
+                </div>
+
+                <div className='md:hidden'>
+                    <button onClick={toggleMenu} className='text-white  text-xl focus:outline-none focus:text-gray-300'>
+                        {isMenuOpen ? <FaXmark className='w-6 h-6 text-primary ' /> : <FaBars className='w-6 h-6 text-primary' />}
+                    </button>
+                </div>
+            </div>
+
+            {/* Responsive Menu */}
+            <div className={`md:hidden ${isMenuOpen ? 'block fixed top-16 right-0 left-0 z-50' : 'hidden space-y-4 px-4 pt-24 pb-5 bg-secondary'}`}>
+                <ul className='flex flex-col space-y-4 px-4 pt-24 pb-5 bg-secondary'>
+                    {navItems.map(({ link, path }) => (
+                        <li key={link}>
+                            <Link activeClass='active' spy={true} smooth={true} offset={-80} className='block text-white hover:text-gray-300' to={path}>{link}
+                                onClick ={toggleMenu}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
