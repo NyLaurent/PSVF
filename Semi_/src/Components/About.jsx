cb397aaa091adf42f46db0a35c15ed8d2dc79497
lauLanez <laulanyumbayire@gmail.com>
2024-07-03 12:50:40 -0500

    addd new feature

--- Semi_/src/Components/About.jsx
+++ Semi_/src/Components/About.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { FaCircleCheck } from "react-icons/fa6";
+import house from '../assets/house.png'
 
 const About = () => {
     return (
         <div>
-        <p>jfdagh</p>
+            <div className='flex justify-center'>
+            <div className='w-[700px] h-[58px] bg-[white] shadow-lg relative bottom-6 rounded-lg'>
+            <ul className='flex justify-evenly pt-3'>
+                <li className='flex gap-2.5 '><li><FaCircleCheck className='text-[#325AAB]'/></li> Ordinary level</li>
+                <li className='flex gap-2.5'> <li><FaCircleCheck className='text-[#325AAB]'/></li>Advanced level</li>
+                <li className='flex gap-2.5'> <li><FaCircleCheck className='text-[#325AAB]'/></li>Lorem Ipsum</li>
+            </ul>
+        </div>
+            </div>
+
+            <div>
+                <img src={house} alt="" />
+            </div>
+       
         </div>
     );
 };
